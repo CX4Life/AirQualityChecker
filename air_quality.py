36e4bc6b23ac8c8e7c8d52a61026395faf7990fc
Tim Woods <tim.woods.tw@gmail.com>
2018-10-13 16:22:37 -0700

    Pushing small changes before starting new project

--- air_quality.py
+++ air_quality.py
@@ -14,8 +14,13 @@ bellingham_site_url = 'https://fortress.wa.gov/ecy/enviwa/StationInfo.aspx?ST_ID
 class AQI:
     def __init__(self, site_url):
         self.air_quality_site_url = site_url
+        self.start_time = None
         self.times = []
         self.scores = []
+
+    @staticmethod
+    def _seconds_from_datetime(date_time):
+        return (((date_time.hour * 60) + date_time.minute) * 60) + date_time.second
 
     def report(self):
         plt.plot(self.times, self.scores, '-ro')
@@ -25,10 +30,15 @@ class AQI:
     def scrape(self):
         aqi_page = requests.get(self.air_quality_site_url)
         soup = bs4.BeautifulSoup(aqi_page.content, 'html.parser')
-        current_score = int(soup.find('span', {'id': 'lblCurrentIndex'}).text)
-        now = datetime.now().time()
-        self.times.append((((now.hour * 60) + now.minute) * 60) + now.second)
-        self.scores.append(current_score)
+        try:
+            current_score = int(soup.find('span', {'id': 'lblCurrentIndex'}).text)
+            now = datetime.now().time()
+            if self.start_time is None:
+                self.start_time = self._seconds_from_datetime(now)
+            self.times.append(self.start_time - self._seconds_from_datetime(now))
+            self.scores.append(current_score)
+        except AttributeError:
+            pass
 
 
 def main():
